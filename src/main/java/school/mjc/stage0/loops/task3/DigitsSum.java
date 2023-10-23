119a6ee1155d87b1ec1cf5e0e172a533d59454c7
8ar7ar <5ar7ar@mail.ru>
2023-10-23 22:05:25 +0500

    stage0-module4-loops-task3 "1st attempt" "locally tested"

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        int sum = 0;
+        int number = t;
+        if (number < 0) number *= -1;
+        while (number > 0){
+            sum += number%10;
+            number /= 10;
+        }
+        System.out.println(sum);
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int num1 = 0;
+        int num2 = 1;
+        int count = 1;
+        while (count <= lastFibonacci) {
+            System.out.println(num1);
+            int temp = num2; //1
+            num2 += num1;  //1 + 0 =1
+            num1 = temp;
+            count++;
+        }
     }
 }
